bbdb46dd32883cd9bbc99022becefa62138a2d6b
David8633 <davidinstituto282@gmail.com>
2025-03-20 12:41:22 +0100

    Creacion de constructor del objeto galaxia, con un condicional para que salte la exception

--- src/com/model/Galaxia.java
+++ src/com/model/Galaxia.java
@@ -3,7 +3,16 @@ package com.model;
 public class Galaxia {
 
 	public Galaxia() {
-
+		private int[][] galaxia;
+	
+	public Galaxia(int numero) throws InvalidNumberException {
+		
+		if(numero%2==0||numero<1) {
+			throw new InvalidNumberException() ;
+		}else {
+			this.galaxia= new  int[numero][numero];
+		}
+		
  
 	}
 
