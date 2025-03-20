43fac6fcbb07a34db77bf0fadf3cd59ec2359dd1
mamugo1432 <mamugo1432@gmail.com>
2025-03-20 10:52:40 +0100

    Añadiendo la estructura del proyecto

---
+++ src/com/model/Galaxia.java
@@ -0,0 +1,9 @@
+package com.model;
+
+public class Galaxia {
+
+	public Galaxia() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ tests/com/modelTests/GalaxiaTests.java
@@ -0,0 +1,14 @@
+package com.modelTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GalaxiaTests {
+
+	@Test
+	void testGalaxia() {
+		fail("Not yet implemented");
+	}
+
+}
