7b3dda3abb59e372f79ce5042ae29049da4d23b3
yagofb <yagofernandezblandon1@gmail.com>
2025-03-20 13:21:32 +0100

    Se ha hecho el test de Galaxia

--- tests/com/modelTests/GalaxiaTests.java
+++ tests/com/modelTests/GalaxiaTests.java
@@ -1,14 +1,29 @@
 package com.modelTests;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import com.model.Galaxia;
 class GalaxiaTests {
-
-	@Test
-	void testGalaxia() {
-		fail("Not yet implemented");
-	}
-
+	
+	class TestGalaxia {
+		private static Galaxia Galaxia1;
+		private static Galaxia Galaxia2;
+		private static Galaxia Galaxia3;
+		private static Galaxia Galaxia4;
+		private static Galaxia Galaxia5;
+		
+		@BeforeAll
+		static void crearGalaxias() {
+			try{
+				Galaxia1 = new Galaxia(5);
+				Galaxia2 = new Galaxia(7);
+				Galaxia3 = new Galaxia(9;
+				Galaxia4 = new Galaxia(11);
+				Galaxia5 = new Galaxia(13);
+			}catch(InvalidNumberException e) {
+				System.out.println(e.getLocalizedMessage());
+			}
+			}
+		@Test
+		 void objetoLanzaExcepcion() {
+			assertThrows(InvalidNumberException.class, () -> new Galaxia(6));
 }
