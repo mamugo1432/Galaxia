5addf4e34fc7d9eae158e34f03cc3bfa10e49e78
David8633 <davidinstituto282@gmail.com>
2025-03-20 18:24:17 +0100

    añado el constructor Galaxia en la parte del main, con la exeception para que salte y si es par o es menor a 1

--- src/com/model/Galaxia.java
+++ src/com/model/Galaxia.java
@@ -4,7 +4,17 @@ public class Galaxia {
 
 	public Galaxia() {
 		
+		private int[][] galaxia;
+	
+	public Galaxia(int numero) throws InvalidNumberException {
 		
+		if(numero%2==0||numero<1) {
+			throw new InvalidNumberException() ;
+		}else {
+			this.galaxia= new  int[numero][numero];
+		}
+		
+
 		
 	}
 	protected int moverAbajo(int posicionVertical) {
