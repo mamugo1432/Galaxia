10c23bd1b515a0f8c032c969e4d23577ca0e3dd9
Elias Molina <elias.m.v.2004@gmail.com>
2025-03-21 20:58:12 +0100

    Arreglando metodos y demas

--- src/com/model/Galaxia.java
+++ src/com/model/Galaxia.java
@@ -6,13 +6,22 @@ import java.util.Random;
 public class Galaxia {
 	
 	private int[][] galaxia;
+	private int posicionColumna;
+	private int posicionFila;
 	
 	public Galaxia(int numero) throws InvalidNumberException {
 		Random rd = new Random();
 		if(numero%2==0||numero<1) {
 			throw new InvalidNumberException() ;
 		}else {
-			this.galaxia= new  int[numero][numero];
+			this.galaxia= new int[numero][numero];
+			for (int i = 0; i < galaxia.length; i++) {
+				for (int j = 0; j < galaxia[0].length; j++) {
+					galaxia[i][j]=rd.nextInt(9)+1;
+				}
+			}
+			this.posicionColumna = calcularMedio(galaxia);
+			this.posicionFila = calcularMedio(galaxia);
 		}	
 	}
 	
@@ -25,51 +34,50 @@ public class Galaxia {
 			}	
 		}
 	
-	protected int moverAbajo(int posicionVertical) {
+	private int moverAbajo(int posicionVertical) {
 
-		return posicionVertical--;
+		return posicionVertical+1;
 	}
 
-	protected int moverIzquierda(int posicionHorizontal) {
+	private int moverIzquierda(int posicionHorizontal) {
 
-		return posicionHorizontal--;
+		return posicionHorizontal-1;
 	}
 
-	protected int moverDerecha(int posicionHorizontal) {
-		
-		return posicionHorizontal++;
-	}
-	
-	protected int moverArriba(int posicionVertical) {
-		
-		return posicionVertical++;
+	private int moverDerecha(int posicionHorizontal) {
+		return posicionHorizontal+1;
 	}
 	
+	private int moverArriba(int posicionVertical) {
+		
+		return posicionVertical-1;
+	}
+	
 
-	protected int contarLinea(int posicionColumna , int posicionFila ,int direccion,int movimiento) {
+	private int contarLinea(int direccion,int movimiento) {
 		int suma = 0;
 		// Movimiento hacia la derecha
-	    if (direccion == 0) {
-	        for (int col = posicionColumna; col < movimiento; col++) {
-	            suma += galaxia[moverDerecha(posicionColumna)][col];
+	    if (direccion == 1) {
+	        for (int fila = 0; fila < movimiento; fila++) {
+	            suma += galaxia[posicionColumna][posicionFila=moverDerecha(posicionFila)];
 	        }
 	    } 
 	    // Movimiento hacia abajo
-	    else if (direccion == 1) {
-	        for (int fila = posicionFila; fila < movimiento; fila++) {
-	            suma += galaxia[fila][moverAbajo(posicionFila)];
+	    else if (direccion == 2) {
+	        for (int col = 0; col < movimiento; col++) {
+	            suma += galaxia[posicionColumna=moverAbajo(posicionColumna)][posicionFila];
 	        }
 	    } 
 	    // Movimiento hacia la izquierda
-	    else if (direccion == 2) {
-	        for (int col = galaxia.length - 1; col >= 0; col--) {
-	            suma += galaxia[moverIzquierda(posicionColumna)][col];
+	    else if (direccion == 3) {
+	        for (int fila = movimiento; fila > 0; fila--) {
+	            suma += galaxia[posicionColumna][posicionFila=moverIzquierda(posicionFila)];
 	        }
 	    } 
 	    // Movimiento hacia arriba
-	    else if (direccion == 3) {
-	        for (int fila = galaxia.length - 1; fila >= 0; fila--) {
-	            suma += galaxia[fila][moverArriba(posicionFila)];
+	    else if (direccion == 0) {
+	        for (int col = movimiento; col > 0; col--) {
+	            suma += galaxia[posicionColumna=moverArriba(posicionColumna)][posicionFila];
 	        }
 	    }
 	    
@@ -80,27 +88,24 @@ public class Galaxia {
 	public int contarEstrellas() {
 		int direccion = 0;
 		int movimiento = 1;
-		int posicionColumna = calcularMedio(this.galaxia);
-		int posicionFila = calcularMedio(this.galaxia);
 		int sumaTotal = galaxia[posicionFila][posicionColumna];
 		boolean parar = false;
-		while (parar = false) {
+		while (parar == false) {
 			try {
-				sumaTotal += contarLinea(posicionColumna, posicionFila, direccion, movimiento);
+				sumaTotal += contarLinea(direccion, movimiento);
 			} catch (ArrayIndexOutOfBoundsException e) {
-				sumaTotal += contarLinea(posicionColumna, posicionFila, direccion, movimiento-1);
+				sumaTotal += contarLinea(direccion, movimiento-1);
 				parar=true;
 			}
-			direccion++;
+			direccion = (direccion>=3) ? 0:direccion+1;
 			movimiento++;
 		}
 		return sumaTotal;
 	}
 	
-	protected int calcularMedio(int[][] matriz) {
+	private int calcularMedio(int[][] matriz) {
 		return (matriz.length-1)/2;		
 	}
-
 
 	@Override
 	public String toString() {
