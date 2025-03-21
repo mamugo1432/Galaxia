54f9f651ba0970f28cf9e33ba0d35de2797e8051
Elias Molina <elias.m.v.2004@gmail.com>
2025-03-21 20:04:09 +0100

    Subiendo constructor nuevo y toString arreglado

--- tests/com/modelTests/GalaxiaTests.java
+++ tests/com/modelTests/GalaxiaTests.java
@@ -52,47 +52,48 @@ class GalaxiaTests {
 			{2,4,3,1}
 	};
 		
-		@BeforeAll
-		static void crearGalaxias() {
-			try{
-				Galaxia1 = new Galaxia(5);
-				Galaxia2 = new Galaxia(7);
-				Galaxia3 = new Galaxia(9);
-				Galaxia4 = new Galaxia(11);
-				Galaxia5 = new Galaxia(13);
-			}catch(InvalidNumberException e) {
-				System.out.println(e.getLocalizedMessage());
-			}
-			}
-		@Test
-		 void objetoLanzaExcepcion() {
-			assertThrows(InvalidNumberException.class, () -> new Galaxia(6));
+	@BeforeAll
+	static void crearGalaxias() {
+		try{
+			Galaxia1 = new Galaxia(5);
+			Galaxia2 = new Galaxia(7);
+			Galaxia3 = new Galaxia(9);
+			Galaxia4 = new Galaxia(11);
+			Galaxia5 = new Galaxia(13);
+		}catch(InvalidNumberException e) {
+			System.out.println(e.getLocalizedMessage());
 		}
-		
-		@Test
-		void moverDerechaTest() {
-			  assertEquals(6, Galaxia1.moverDerecha(5));
-		      assertEquals(1, Galaxia1.moverDerecha(0));
-		      assertEquals(-4, Galaxia1.moverDerecha(-5));
+	}
+	
+	@Test
+	 void objetoLanzaExcepcion() {
+		assertThrows(InvalidNumberException.class, () -> new Galaxia(6));
+	}
+	
+	@Test
+	void moverDerechaTest() {
+		  assertEquals(6, Galaxia1.moverDerecha(5));
+	      assertEquals(1, Galaxia1.moverDerecha(0));
+	      assertEquals(-4, Galaxia1.moverDerecha(-5));
+	      
+	      assertEquals(6, Galaxia2.moverDerecha(5));
+	      assertEquals(1, Galaxia2.moverDerecha(0));
+	      assertEquals(-4, Galaxia2.moverDerecha(-5));
+	      
+	      assertEquals(6, Galaxia3.moverDerecha(5));
+	      assertEquals(1, Galaxia3.moverDerecha(0));	
+	      assertEquals(-4, Galaxia3.moverDerecha(-5));
 		      
-		      assertEquals(6, Galaxia2.moverDerecha(5));
-		      assertEquals(1, Galaxia2.moverDerecha(0));
-		      assertEquals(-4, Galaxia2.moverDerecha(-5));
-		      
-		      assertEquals(6, Galaxia3.moverDerecha(5));
-		      assertEquals(1, Galaxia3.moverDerecha(0));
-		      assertEquals(-4, Galaxia3.moverDerecha(-5));
-		      
-		      assertEquals(6, Galaxia4.moverDerecha(5));
-		      assertEquals(1, Galaxia4.moverDerecha(0));
-		      assertEquals(-4, Galaxia4.moverDerecha(-5));
-		      
-		      assertEquals(6, Galaxia5.moverDerecha(5));
-		      assertEquals(1, Galaxia5.moverDerecha(0));
-		      assertEquals(-4, Galaxia5.moverDerecha(-5));
+		  assertEquals(6, Galaxia4.moverDerecha(5));
+		  assertEquals(1, Galaxia4.moverDerecha(0));
+		  assertEquals(-4, Galaxia4.moverDerecha(-5));
+		     
+		  assertEquals(6, Galaxia5.moverDerecha(5));
+		  assertEquals(1, Galaxia5.moverDerecha(0));
+		  assertEquals(-4, Galaxia5.moverDerecha(-5));
 		}
 
-		@Test
+	@Test
 	void testContarEstrellas(){
 		assertEquals(41,ContarEstrellas(matriz5));
 		assertEquals(54, ContarEstrellas(matriz7));
