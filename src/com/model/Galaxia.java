8e4f24f95e13926e57fd53b1b4408608efe19cb9
Miguel Ángel Larinto <147139169+MiguelLarintoo@users.noreply.github.com>
2025-03-21 14:24:06 +0100

    Se ha hecho el metodo de contar Lineas

--- src/com/model/Galaxia.java
+++ src/com/model/Galaxia.java
@@ -37,6 +37,40 @@ public class Galaxia {
 		
 		return posicionVertical++;
 	}
+	
+	public int contarLinea(int movimiento,int direccion, boolean bandera) {
+		int suma = 0;
+		bandera = true;
+		// Movimiento hacia la derecha
+	    if (direccion == 0) {
+	        for (int col = 0; col < galaxia.length; col++) {
+	            suma += galaxia[movimiento][col];
+	        }
+	    } 
+	    // Movimiento hacia abajo
+	    else if (direccion == 1) {
+	        for (int fila = 0; fila < galaxia.length; fila++) {
+	            suma += galaxia[fila][movimiento];
+	        }
+	    } 
+	    // Movimiento hacia la izquierda
+	    else if (direccion == 2) {
+	        for (int col = galaxia.length - 1; col >= 0; col--) {
+	            suma += galaxia[movimiento][col];
+	        }
+	    } 
+	    // Movimiento hacia arriba
+	    else if (direccion == 3) {
+	        for (int fila = galaxia.length - 1; fila >= 0; fila--) {
+	            suma += galaxia[fila][movimiento];
+	        }
+	    }else {
+        	bandera = false;
+        }
+        
+        return suma;
+	}
+	
 
 	@Override
 	public String toString() {
